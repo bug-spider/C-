b675eb3326bc9a36efca1c4065ff3f1a85c0e6fd
bug-spider <103446339+bug-spider@users.noreply.github.com>
2022-05-06 14:57:29 +0800

    Create 加法计算器.c

---
+++ 加法计算器.c
@@ -0,0 +1,16 @@
+//c语言函数，加法计算器
+#include<stdio.h>
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = 10;
+	printf("%d",sum);
+	return 0;
+}
